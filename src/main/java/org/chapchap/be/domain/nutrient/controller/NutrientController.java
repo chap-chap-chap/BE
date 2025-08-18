603b56139efa8df01937ca50b74ba0198a6a96c6
Yugyeong Park <bonbonly7@naver.com>
2025-08-19 00:08:13 +0900

    fix: 사료 영상소 체크 API에 Swagger 적용

--- src/main/java/org/chapchap/be/domain/nutrient/controller/NutrientController.java
+++ src/main/java/org/chapchap/be/domain/nutrient/controller/NutrientController.java
@@ -1,5 +1,7 @@
 package org.chapchap.be.domain.nutrient.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.chapchap.be.domain.nutrient.dto.NutrientRequest;
 import org.chapchap.be.global.config.PythonApiClient;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Nutrient")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -16,8 +19,10 @@ public class NutrientController {
 
     private final PythonApiClient pythonApiClient = new PythonApiClient("http://fastapi:8000");
 
+    @Operation(summary = "사료 영양소 체크")
     @PostMapping("/check_nutrient")
     public ResponseEntity<String> checkNutrient(@RequestBody NutrientRequest request) {
+        // 사료의 영양소 적정 여부를 FastAPI 서버로부터 응답
         String result = pythonApiClient.checkNutrient(request.getNutrient(), request.getValue());
         return ResponseEntity.ok(result);
     }
--- src/main/java/org/chapchap/be/domain/nutrient/dto/NutrientRequest.java
+++ src/main/java/org/chapchap/be/domain/nutrient/dto/NutrientRequest.java
@@ -1,9 +1,14 @@
 package org.chapchap.be.domain.nutrient.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+@Schema(description = "영양소 요청 DTO")
 @Data
 public class NutrientRequest {
+    @Schema(description = "영양소 이름", example = "단백질")
     private String nutrient;
+
+    @Schema(description = "영양소 값", example = "25.0")
     private double value;
 }
