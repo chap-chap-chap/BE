de5dd2e3508cdceea77b38cebfa3c68dca2aeaee
Yugyeong Park <bonbonly7@naver.com>
2025-08-18 15:05:20 +0900

    delete: 카카오 맵을 이용한 경로 추천 로직 삭제

--- src/main/java/org/chapchap/be/domain/map/dto/KakaoCoordAddressDoc.java
+++
@@ -1,9 +0,0 @@
-package org.chapchap.be.domain.map.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-// 좌표 → 주소
-public record KakaoCoordAddressDoc(
-        KakaoAddressDoc.Address address,
-        @JsonProperty("road_address") KakaoAddressDoc.RoadAddress roadAddress
-) {}
--- src/main/java/org/chapchap/be/domain/map/dto/KakaoTransCoordDoc.java
+++
@@ -1,7 +0,0 @@
-package org.chapchap.be.domain.map.dto;
-
-// 좌표계 변환
-public record KakaoTransCoordDoc(
-        Double x, // 변환된 경도
-        Double y  // 변환된 위도
-) {}
